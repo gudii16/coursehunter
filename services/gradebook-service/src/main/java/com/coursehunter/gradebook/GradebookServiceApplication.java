bab994027a00be81fa7ac3f400ad769bd970701c
ROHAN KUMAR <maxphinix@gmail.com>
2026-05-03 11:05:44 +0000

    Added docker file and basic springboot app in all the services.

---
+++ services/gradebook-service/src/main/java/com/coursehunter/gradebook/GradebookServiceApplication.java
@@ -0,0 +1,12 @@
+package com.coursehunter.gradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradebookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradebookServiceApplication.class, args);
+    }
+}
